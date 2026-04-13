8e2a2757e6223101440cd5a7d80a37b8824c697e
gaavendano <magicgabbynyc@gmail.com>
2026-04-13 13:39:05 -0500

    Initial Commit

---
+++ Pokemon App/Pokemon_AppApp.swift
@@ -0,0 +1,32 @@
+//
+//  Pokemon_AppApp.swift
+//  Pokemon App
+//
+//  Created by Gabby A on 4/13/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Pokemon_AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Pokemon AppTests/Pokemon_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  Pokemon_AppTests.swift
+//  Pokemon AppTests
+//
+//  Created by Gabby A on 4/13/26.
+//
+
+import Testing
+@testable import Pokemon_App
+
+struct Pokemon_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
